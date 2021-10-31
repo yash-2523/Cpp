1f5975c52b0f87c4829861c6e2137aa2ad648fe4
yash-2523 <56871771+yash-2523@users.noreply.github.com>
2021-10-31 11:58:11 +0530

    Create Sample.cpp

---
+++ Sample.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef unsigned long long int ull;
+#define mp make_pair
+#define pb push_back
+#define MOD 1000000007
+#define f(i,n) for(ll i=0;i<(ll)n;i++)
+#define fs(i,a,b) for(int i=a;i<b;i++)
+#define fr(i,a,b) for(int i=a;i>=b;i--)
+#define c(a) cin >> a
+#define all(a) a.begin(),a.end()
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define de cerr
+#ifdef LOCAL
+#define cerr cout
+#endif
+using namespace std;
+
+int main(){
+  fast;
+  
+  return 0;
+}
